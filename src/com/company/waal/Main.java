33432df38219361316ee2a80ad7c4bc84254b30a
wasim <you@example.com>
2021-03-06 18:31:46 +0530

    nothing

--- src/com/company/waal/Main.java
+++ src/com/company/waal/Main.java
@@ -3,6 +3,7 @@ package com.company.waal;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("WAAL fun write in here");
         System.out.println("Welcome in WAAL");
     }
 }
